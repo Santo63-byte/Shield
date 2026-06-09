7704a57f38f17b94a03ec2bda5815d4c25f44529
Sbytes <santoshaji63@gmail.com>
2026-06-09 23:57:01 +0530

    Feature/SHD-003-Anonymous User entry should throw ShieldException

--- shield-core/src/main/java/com/sbyte/shield/core/services/authenticator/support/AuthSupport.java
+++ shield-core/src/main/java/com/sbyte/shield/core/services/authenticator/support/AuthSupport.java
@@ -1,11 +1,13 @@
 package com.sbyte.shield.core.services.authenticator.support;
 
-import com.sbyte.shield.dto.UserSessionDTO;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import com.sbyte.shield.core.exceptions.ShieldExceptions;
+import com.sbyte.shield.dto.UserSessionDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Component("authSupport")
 public class AuthSupport {
@@ -16,10 +18,12 @@ public class AuthSupport {
     @Autowired
     HttpServletRequest request;
 
-    public Authentication getAuthenticationStatus() {
+    public Authentication getAuthenticationStatus() throws ShieldExceptions {
         Authentication authentication = jwtTokenProvider.getAuthenticationFromContext();
         assert authentication != null;
-        authentication.setAuthenticated(authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken));
+        if(authentication instanceof org.springframework.security.authentication.AnonymousAuthenticationToken) {
+            throw new ShieldExceptions("Anonymous User","No authenticated user found in the current context",401);
+        }
         return authentication;
     }
     public UserSessionDTO getCurrentUserSession() {
